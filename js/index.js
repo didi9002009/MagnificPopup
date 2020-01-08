aa65706b4f74dc369ec820455597c424f00325f3
Ronald Laura <rsl950@newcastle.edu.au>
2020-01-08 21:38:37 +1100

    update all

---
+++ js/index.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $('.parent-container').magnificPopup({
+    delegate: 'a', // child items selector, by clicking on it popup will open
+    type: 'image',
+    gallery:{
+      enabled:true
+    }
+  });
+});
+
+$('.image-link').magnificPopup({
+  type: 'image'
+  // other options
+});
