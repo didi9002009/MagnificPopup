1e4f14711c3c0ffaefbd447c367edf245c482999
didi9002009 <53850889+didi9002009@users.noreply.github.com>
2020-02-20 13:44:54 +1100

    Commit-fixed the problem

--- js/index.js
+++ js/index.js
@@ -1,14 +1,15 @@
+/*eslint-env jquery*/
 $(document).ready(function() {
-  $('.parent-container').magnificPopup({
-    delegate: 'a', // child items selector, by clicking on it popup will open
-    type: 'image',
-    gallery:{
-      enabled:true
+  $(".parent-container").magnificPopup({
+    delegate: "a", // child items selector, by clicking on it popup will open
+    type: "image",
+    gallery: {
+      enabled: true
     }
   });
 });
 
-$('.image-link').magnificPopup({
-  type: 'image'
+$(".image-link").magnificPopup({
+  type: "image"
   // other options
 });
